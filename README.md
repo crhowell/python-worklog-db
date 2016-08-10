fdc0ff6e15686bb1dc80a728da00e387d01b0308
Chris H <crhowell1@gmail.com>
2016-08-09 19:15:00 -0700

    Update README
    Add debug select for all tasks

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# python-worklog-db
-A Python terminal application for logging timesheets in a database.
+# Python WorkLog with Database
+A Python terminal application that uses a database to store timesheets.
+
+## Prerequisites
+- Python 3
+- Peewee
+
+## To Run
+1. Open a command-line or terminal window.
+2. `cd` into the project directory
+3. Type the following in the project directory:
+    `python app.py`
--- log.py
+++ log.py
@@ -4,17 +4,23 @@ import settings
 
 class Log:
 
+    def all_tasks(self):
+        tasks = Task.select().order_by(Task.date.desc())
+        return tasks
+
     def add_task(self):
         emp_name = input(' Employee Name: ')
         t_name = input(' Task Name: ')
         t_time = input(' Time Spent (in minutes): ')
         date = input(' Task Date: ')
         notes = input(' Notes: ')
-        task = Task.create(employee=emp_name,
-                           name=t_name,
-                           mins=t_time,
-                           date=date,
-                           notes=notes)
+        task = Task.create(
+            employee=emp_name,
+            name=t_name,
+            mins=t_time,
+            date=date,
+            notes=notes
+        )
 
     def connect(self):
         self.db.connect()
