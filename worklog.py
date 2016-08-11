85888373d06ec10ac567cd879f3584396275096f
Chris H <crhowell1@gmail.com>
2016-08-11 02:06:53 -0700

    Add display task and pagination

--- worklog.py
+++ worklog.py
@@ -15,12 +15,41 @@ class WorkLog:
             self.get_main_selection(menu_choice)
             self.clear_screen()
 
+    def display_paginated(self, tasks=[]):
+        if tasks:
+            i = 0
+            while True:
+                self.clear_screen()
+                page_dir = self.allowable_page_dir(i, len(tasks))
+                choices = [key for key in page_dir]
+                self.display_task_count(i+1, len(tasks))
+                self.display_task(tasks[i])
+                prompt = ' | '.join([page_dir[key] for key in choices])
+                choice = self.prompt_menu_choice(choices, '[{}]: '.format(prompt))
+                if choice == 'p':
+                    i -= 1
+                elif choice == 'n':
+                    i += 1
+                elif choice == 'e':
+                    # TODO
+                    print('edit placeholder...')
+                    break
+                elif choice == 'd':
+                    # TODO
+                    print('delete placeholder...')
+                    break
+                elif choice == 'q':
+                    break
+
     def get_main_selection(self, choice=''):
         if choice:
             if choice == 'a':
                 self.log.add_task()
                 self.prompt_action_status('Task Added')
                 self.clear_screen()
+            elif choice == 'f':
+                tasks = self.log.all_tasks()
+                self.display_paginated(tasks)
 
     @staticmethod
     def prompt_action_status(prompt='\n'):
@@ -63,6 +92,18 @@ class WorkLog:
             print('\n Task {} of {}\n'.format(curr, count))
 
     @staticmethod
+    def display_task(task=None):
+        if task is not None:
+            print('=' * 45)
+            print(' Task Name: {}\n Minutes Spent: {}\n Notes: {}\n Date: {}'.format(
+                task.name,
+                task.mins,
+                task.notes,
+                task.date
+            ))
+            print('=' * 45)
+
+    @staticmethod
     def display_main_menu():
         """Displays to terminal the Main Menu selections."""
         print(' What would you like to do? ')
@@ -89,6 +130,32 @@ class WorkLog:
         """
         return datetime.strptime(date, fmt).strftime(
             settings.DATE_FORMAT)
+
+    @staticmethod
+    def allowable_page_dir(num, size):
+        """Returns list of allowable pagination directions.
+
+        Keyword arguments:
+        num -- Current task number
+        size -- Size of the list of tasks
+        """
+        choices = {
+            'e': '(E)dit',
+            'd': '(D)elete',
+            'p': '(P)revious',
+            'n': '(N)ext',
+            'q': '(Q)uit'
+        }
+        if 0 < num < size - 1:
+            return choices
+        elif num == 0:
+            del choices['p']
+            if num == size - 1:
+                del choices['n']
+            return choices
+        else:
+            del choices['n']
+            return choices
 
     def __init__(self):
         self.log = Log()
